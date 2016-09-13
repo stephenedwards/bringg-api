b1e98d9e8b558a9c6474126b789c4b97c5813e58
Stephen Edwards <stephen.edwards@foostix.lu>
2016-09-13 17:09:43 +0200

    initial commit

---
+++ bring-api.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'bringg-api'
+  s.version     = '0.0.1'
+  s.date        = '2016-09-13'
+  s.summary     = "Hola!"
+  s.description = "A simple hello world gem"
+  s.authors     = ["Stephen Edwards"]
+  s.email       = 'stephen.edwards@foostix.com'
+  s.files       = ["lib/bringg-api.rb"]
+  s.homepage    =
+    'https://www.foostix.com'
+  s.license       = 'MIT'
+end
---
+++ lib/bringg-api.rb
@@ -0,0 +1,66 @@
+require 'net/http'
+require 'json'
+module BringgApi
+  
+  class Options
+    @action_urls = {}    
+    def self.set_action_url(args = {})      
+      if args.class != Hash
+        raise BringgApiException::IncorrectParams, "Expecting Hash as parameter: :action => 'url'"
+        return
+      end
+      args.each do |k, v|
+        @action_urls[k] = v
+      end
+      
+      
+      return true
+    end
+    
+    def self.get_action_url(_action)
+      if @action_urls.has_key?(_action)
+        @action_urls[_action]
+      else
+        nil  
+      end
+    end
+  end  
+     
+  class CreateTask
+    def initialize
+      action_url = Options.get_action_url(:create_task)
+      if action_url.nil?
+        raise BringgApiException::ActionNotDefined, "Action URL for 'create_task' not set"
+        return
+      end
+      @url = action_url
+      @params = {}
+    end
+    
+    def set_params(args = {})
+      args.each do |k,v|
+        @params[k] = v
+      end
+      return true
+    end
+    
+    def send
+      uri = URI(@url)
+      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      req.body = @params.to_json
+      http = Net::HTTP.new(uri.hostname, uri.port)
+      http.use_ssl = true
+      res = http.request(req)      
+    end
+  end
+  
+  
+  module BringgApiException
+    class ActionNotDefined < Exception
+    end
+    class IncorrectParams < Exception 
+    end
+    class MissingParams < Exception 
+    end
+  end
+end

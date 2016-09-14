76e7426c83d1af6d579f11fdf801fa2ec1fbab01
Stephen Edwards <stephen.edwards@foostix.lu>
2016-09-14 11:42:08 +0200

    convert service calls to dev. api calls

--- lib/bringg-api.rb
+++ lib/bringg-api.rb
@@ -1,40 +1,22 @@
 require 'net/http'
 require 'json'
+require 'openssl'
 module BringgApi
   
-  class Options
-    @action_urls = {}    
-    def self.set_action_url(args = {})      
-      if args.class != Hash
-        raise BringgApiException::IncorrectParams, "Expecting Hash as parameter: :action => 'url'"
-        return
-      end
-      args.each do |k, v|
-        @action_urls[k] = v
-      end
-      
-      
-      return true
-    end
-    
-    def self.get_action_url(_action)
-      if @action_urls.has_key?(_action)
-        @action_urls[_action]
-      else
-        nil  
-      end
-    end
-  end  
-     
-  class CreateTask
+  @options = {}
+  class << self
+    attr_accessor :options
+  end
+  
+  class BringgAction
     def initialize
-      action_url = Options.get_action_url(:create_task)
-      if action_url.nil?
-        raise BringgApiException::ActionNotDefined, "Action URL for 'create_task' not set"
-        return
-      end
-      @url = action_url
       @params = {}
+      required_options = [:company_id, :access_token, :secret_key]
+      required_options.each do |req_option|
+        if !BringgApi.options.has_key?(req_option)
+          raise BringgApi::BringgApiException::MissingOptions, "BringgApi is missing option :"+req_option.to_s
+        end
+      end
     end
     
     def set_params(args = {})
@@ -45,31 +27,71 @@ module BringgApi
     end
     
     def send
+      #Create request
       uri = URI(@url)
       req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      
+      #add timestamp and token to params
+      @params[:timestamp] = Time.now().to_i
+      @params[:access_token] = BringgApi.options[:access_token]
+      @params[:company_id] = BringgApi.options[:company_id]
+      tmpQuery = URI.encode_www_form(@params)
+      p tmpQuery
+      #sign params      
+      @params[:signature] = OpenSSL::HMAC.hexdigest("sha1", BringgApi.options[:secret_key], tmpQuery).to_s
+            
       req.body = @params.to_json
       http = Net::HTTP.new(uri.hostname, uri.port)
       http.use_ssl = (uri.scheme == "https")
       res = http.request(req)
-      j = JSON.parse(res.body)
-      if j["success"]
-        return j ["task"]
+      p res.code
+      p res.message
+      p res.body
+      if res.code == "200"
+        j = JSON.parse(res.body)
+        if j["success"]          
+          return j
+        else
+          raise BringgApiException::ActionError, j["message"]
+        end  
       else
-        raise BringgApiException::ErrorCreatingTask, j["message"]
-      end
-    end
-  end
-  
-  
-  module BringgApiException
-    class ActionNotDefined < Exception
-    end
-    class IncorrectParams < Exception 
-    end
-    class MissingParams < Exception 
-    end
-    class ErrorCreatingTask < Exception
+        raise BringgApiException::HTTPError, res.code.to_s + " " + res.message
+      end      
       
     end
+    
+  end
+    
+  class CreateTask < BringgAction
+    def initialize
+      @url = "https://developer-api.bringg.com/partner_api/tasks"      
+      super
+    end   
+    
+    def send
+      super["task"]
+    end
+    
+  end
+  
+  class CreateTaskWithWayPoints < BringgAction
+    def initialize
+      @url = "https://developer-api.bringg.com/partner_api/tasks/create_with_way_points"      
+      super
+    end
+    
+    def send
+      super["task"]
+    end
+     
+  end
+  
+  module BringgApiException
+    class MissingOptions < Exception      
+    end
+    class ActionError < Exception      
+    end
+    class HTTPError < Exception
+    end            
   end
 end
